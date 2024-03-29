113f5bd30d81ff5d9b20a597a3177fa8b7d7f9cd
Lakmina Pramodya Gamage <24446012+lakminagamage@users.noreply.github.com>
2024-03-30 03:55:34 +0530

    fix(Index): fixed a typo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
 import Footer from "@/components/Footer";
 import Partners from "@/components/Partners";
 
