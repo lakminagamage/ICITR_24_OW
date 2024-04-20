42375d85bf88c2d39099e0271e80f223b3246f45
Sankha Bimsara <sankha.b21@gmail.com>
2024-04-21 01:17:07 +0530

    Comittee Grid Changes

--- src/components/CommitteeGrid.jsx
+++ src/components/CommitteeGrid.jsx
@@ -3,9 +3,15 @@ import PersonCard from "./PersonCard";
 function CommitteeGrid({ people, title }) {
   return (
     <div className="mt-8 mb-24">
-      <h2 className="text-3xl text-center font-semibold text-blue-primary mb-8">
+      <h2 className="text-3xl text-center font-semibold text-blue-primary mb-4">
         {title}
       </h2>
+      {/* hr */}
+      <div
+        className="mx-auto h-0.5 bg-gradient-to-r from-blue-300 to-pink-300 mb-8"
+        style={{ width: "80%" }}
+      ></div>
+      {/* hr */}
       <div className="flex flex-wrap justify-center items-center">
         {people.map((person, index) => (
           <PersonCard
