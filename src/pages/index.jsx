cf2991444ff898b7bc2b09188e5b1e390159bc70
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2024-04-26 22:21:54 +0530

    added components to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
+import Clock from "@/components/Clock";
+import EventDescription from "@/components/EventDescription";
+import PhotoGallery from "@/components/PhotoGallery";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
       <Hero />
+      <Clock />
+      <EventDescription />
+      <PhotoGallery />
     </div>
   );
 }
