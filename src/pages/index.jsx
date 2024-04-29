a3e77510b97d13514cf968e616273c3dfbff5713
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-04-29 11:12:44 +0530

    feat(main) : added partners

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,16 @@
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import Team from "@/components/Team";
+import Partners from "@/components/Partners";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Partners />
       <Team />
       <Footer />
+
     </div>
   );
 }
