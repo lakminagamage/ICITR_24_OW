a122b45f63f8128773bdb18659996f2d64c9b2d5
imnthiyanga <ithiyanga@gmail.com>
2024-12-03 12:58:05 +0530

    update: Added Gold and Platinum partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -48,59 +48,29 @@ function Partners() {
 
       <div className="mx-auto mt-12">
         <h2 className="text-3xl font-bold text-center tracking-tight text-blue-primary sm:text-4xl">
-          Financial Sponsor
+          Financial Sponsors
         </h2>
-        {/* <p className="mt-6 text-lg leading-8 text-center text-gray-primary">
-          Our financial backbone.
-        </p> */}
       </div>
-      <div className="w-9/12 mx-auto py-8 flex flex-col items-center">
-        <div>
+      <div className="w-9/12 mx-auto py-8 flex flex-col items-center space-y-6 md:space-y-0 md:flex-row md:justify-around">
+        <div className="w-full md:w-auto">
+          <img
+            unoptimized="true"
+            src="/img/partners/ncinga.png"
+            alt="Platinum Sponsor"
+            className="w-auto h-20"
+          />
+          <div className='text-lg mt-2 font-semibold text-black text-center'>Platinum Sponsor</div>
+        </div>
+        <div className="w-full md:w-auto">
           <img
             unoptimized="true"
             src="/img/partners/wso2.png"
-            alt="financial sponsor"
+            alt="Gold Sponsor"
             className="w-auto h-20"
           />
+          <div className='text-lg mt-2 font-semibold text-black text-center'>Gold Sponsor</div>
         </div>
       </div>
-
-      {/* <div className="flex flex-col">
-        <div className="flex flex-col md:flex-row justify-between my-10 md:my-20">
-
-          <div className="flex md:basis-2/5 flex-col items-center space-y-2 px-3 mb-10 md:mb-0">
-            <div className="flex flex-row justify-center items-center space-x-3">
-              <div className='basis-1/3'><img unoptimized src="/img/partners/ms.jpg" alt="partner" className=" w-40 h-auto" /></div>
-              <div className='basis-1/3'><img unoptimized src="/img/partners/nvidia.jpeg" alt="partner" className="w-40 h-auto" /></div>
-              <div className='basis-1/3'><img unoptimized src="/img/partners/ms.jpg" alt="partner" className="w-40 h-auto" /></div>
-            </div>
-            
-            <div className='text-lg font-semibold text-black text-center'>Platinum Partners</div>
-          </div>
-
-          <div className="flex md:basis-2/5 flex-col items-center space-y-2 px-3">
-            <div className="flex flex-row justify-center items-center space-x-3">
-              <div className='basis-1/3'><img unoptimized src="/img/partners/ms.jpg" alt="partner" className=" w-40 h-auto" /></div>
-              <div className='basis-1/3'><img unoptimized src="/img/partners/nvidia.jpeg" alt="partner" className="w-40 h-auto" /></div>
-              <div className='basis-1/3'><img unoptimized src="/img/partners/ms.jpg" alt="partner" className="w-40 h-auto" /></div>
-            </div>
-
-            <div className='text-lg font-semibold text-black text-center'>Gold Partners</div>
-          </div>
-        </div>
-
-        <div className='flex flex-col items-center space-y-2 mb-10 md:mb-20'>
-          <div className="grid grid-cols-3 md:grid-cols-6 items-center gap-3">
-            <img unoptimized src="/img/partners/nvidia.jpeg" alt="partner" className="w-40 h-auto" />
-            <img unoptimized src="/img/partners/ms.jpg" alt="partner" className="w-40 h-auto" />
-            <img unoptimized src="/img/partners/nvidia.jpeg" alt="partner" className="w-40 h-auto" />
-            <img unoptimized src="/img/partners/nvidia.jpeg" alt="partner" className="w-40 h-auto" />
-            <img unoptimized src="/img/partners/ms.jpg" alt="partner" className="w-40 h-auto" />
-            <img unoptimized src="/img/partners/nvidia.jpeg" alt="partner" className="w-40 h-auto" />
-          </div>
-          <div className='text-lg font-semibold text-black text-center'>Silver Partners</div>
-        </div>
-      </div> */}
     </div>
   );
 }
